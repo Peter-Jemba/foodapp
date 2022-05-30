60642eb2cd60d18ba921965d4d3ea02a7622f4b1
Peter-Jemba <peter.jemba.ot@gmail.com>
2022-05-30 08:59:31 +0300

    creeting user

---
+++ models/db_config.py
@@ -0,0 +1,14 @@
+import os
+import psycopg2
+
+
+class DBConn:
+
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+
+    def __init__(self):
+        conn = psycopg2.connect(self.DATABASE_URL)
+        conn.autocommit = True
+        self.cursor = conn.cursor()
+        self.cursor.execute(open('bootcamp.sql', 'r').read())
+        print(self.cursor)
